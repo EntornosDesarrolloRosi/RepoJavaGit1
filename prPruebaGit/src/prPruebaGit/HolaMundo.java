8fb0ed2cabe7a3fff437c610e250f57bb0fd9843
Usuario <Usuario@DESKTOP-5551PLH>
2021-04-26 18:28:21 +0200

    sdfaf

--- prPruebaGit/src/prPruebaGit/HolaMundo.java
+++ prPruebaGit/src/prPruebaGit/HolaMundo.java
@@ -4,8 +4,9 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		System.out.println("Hola mundo");
 		System.out.println("Adios");
+		System.out.println("Añadida en copia 1");
 	}	
 
 }
